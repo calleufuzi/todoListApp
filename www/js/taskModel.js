42d00931ce7f13d6b55bf3b5ffc291b2c6eb330a
calleufuzi <breeg.maiden@gmail.com>
2016-05-03 18:22:22 -0300

    Envio Dos Arquivos

---
+++ www/js/taskModel.js
@@ -0,0 +1,25 @@
+function getTask() {
+
+  this.items = [];
+
+  var lista = localStorage.getItem("TaskList");
+
+  if (lista !== null){
+    this.items = angular.fromJson(lista);
+  };
+
+  this.save = function () {
+    var lista = angular.toJson(this.items);
+    localStorage.setItem("TaskList", lista);
+  };
+
+  this.add = function (item) {
+    this.items.push(item);
+  };
+
+  this.remove = function (item) {
+    var pos = this.items.indexOf(item);
+    this.items.splice(pos, 1);
+  };
+
+}
